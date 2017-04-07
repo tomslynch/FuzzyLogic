23b6718a56015230e364ea3aa3244a84fadcee4b
Thomas Lynch <thomaslynch01@gmail.com>
2017-04-07 00:25:01 -0500

    added constants, scan helper functions, cleaned up var inits, moved old code to end until able to use robot. Added degree and distance arrays with line counter

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -10,8 +10,11 @@
 #include <Servo.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+//TODO: keep stack of "vectors"
+//TODO: Tether to line when leaving to hit ball
 //TODO: Rotate Hitter
 //TODO: Hit ball
+
 
 struct Color {
   uint16_t r = 0; 
@@ -58,6 +61,17 @@ const int LEFT_BACKWARD = 7;
 int MOVE = 500; //delay for moving (delay for half second)
 int STEP5 = 50; //delay scale for rotation (delay time to move 5 degrees)
 
+int DIST_UNIT = 0;  //movement unit
+int ROT_UNIT = 5;   //degree unit (5 degrees)
+
+int COLOR_HIGH = 2000;
+int COLOR_LOW = 100;
+
+int DISTANCE_LIMIT = 10;
+
+// color vars
+Color curColor;
+
 //servo vars
 Servo myservo;
 int pos = 0;
@@ -68,12 +82,14 @@ long distanceBottom;
 
 //movement track
 int ballRot = 0; //rotation from line to find ball
-//add movement track
+int redRot = 0;
 
-
+int deg[100];
+int dis[100];
+int lineCounter = 0;
 
 /*
- * START
+ * -------- START --------
  */
 
 void setup() {
@@ -102,83 +118,92 @@ void setup() {
 }
 
 void loop() {
- check();
-// printColor();
- delay(1000);
 }
 
+/**
+ * -------- SENSOR FUNCTIONS --------
+ */
 
-//
-//void check() {
-//  long distanceBottom = (ultrasonicBottom.distanceRead());
-//  Serial.print("BOTTOM: ");
-//  Serial.print(distanceBottom);
-//  Serial.println(" CM");
-//  long distanceTop = (ultrasonicTop.distanceRead());
-//  Serial.print("TOP: ");
-//  Serial.print(distanceTop);
-//  Serial.println(" CM");
-//  if(distanceTop > distanceBottom){
-//    driveForward();
-//  }
-//}
+/*
+ * Color Detection
+ */
 
-
-
-//void driveForward(){
-//	Serial.println("MOVING FORWARD") ; 
-//	digitalWrite(RIGHT_FORWARD, HIGH); 
-//	digitalWrite(LEFT_FORWARD, HIGH) ; 
-//  digitalWrite(RIGHT_BACKWARD, LOW); 
-//  digitalWrite(LEFT_BACKWARD, LOW) ;
-//  analogWrite (RIGHT_BACKWARD, 0) ; 
-//  analogWrite (LEFT_BACKWARD, 0) ;
-//	analogWrite (RIGHT_FORWARD, 150) ; 
-//	analogWrite (LEFT_FORWARD, 150) ;
-//  
-//	delay(100);
-// 
-//  analogWrite (RIGHT_FORWARD, 0) ; 
-//  analogWrite (LEFT_FORWARD, 0) ;
-//  digitalWrite(RIGHT_FORWARD, LOW); 
-//  digitalWrite(LEFT_FORWARD, LOW) ;
-//  move();	
-//}
-
-void printColor(){
-  uint16_t colorTemp, lux;
-  Color curColor;
+void updateColor(){
   tcs.getRawData(&curColor.r, &curColor.g, &curColor.b, &curColor.c);
- long avg = curColor.c/3 ;
- curColor.r - curColor.r/avg;
- curColor.g - curColor.g/avg;
- curColor.b - curColor.b/avg;
+  long avg = curColor.c/3 ;
+  curColor.r = curColor.r/avg;
+  curColor.g = curColor.g/avg;
+  curColor.b = curColor.b/avg;
+  
   Serial.print("R: "); Serial.print(curColor.r, DEC); Serial.print(" ");
   Serial.print("G: "); Serial.print(curColor.g, DEC); Serial.print(" ");
   Serial.print("B: "); Serial.print(curColor.b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(curColor.c, DEC); Serial.print(" ");
   Serial.println(" ");
 }
-
+// 1 if sees red, else 0
 int isRed(Color &curColor){
-  return (curColor.r > 2000 && curColor.g < 100 && curColor.b < 100);
+  updateColor();
+  if (curColor.r > COLOR_HIGH && curColor.g < COLOR_LOW && curColor.b < COLOR_LOW) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-//void move() {
-//  for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    Serial.println("HEER");
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(100);                       // waits 15ms for the servo to reach the position
-//  }
-//  
-//}
 
 /*
+ * Ball Detection
+ */
+
+// Return 1 if found in 360 around robot, else 0
+int findBall(){
+  int found = seeBall();
+  ballRot = 0;
+  
+  //rotate while checking ball  
+  while(!found || ballRot == 360){
+    rotateOne();
+    ballRot += ROT_UNIT;
+    found = seeBall();
+  }
+
+  if (found && ballRot < 360) {
+    return 1;   
+  } else {
+    return 0;
+  }
+}
+//return 1 if radar input sees ball, else 0
+int seeBall(){
+  distanceTop = (ultrasonicTop.distanceRead());
+  distanceBottom = (ultrasonicBottom.distanceRead());
+  printRadar();
+
+  if (distanceTop - distanceBottom >= DISTANCE_LIMIT) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+void printRadar() {
+  Serial.print("TOP: ");
+  Serial.print(distanceTop);
+  Serial.println(" CM");
+  Serial.print("BOTTOM: ");
+  Serial.print(distanceBottom);
+  Serial.println(" CM");
+
+  if (distanceTop - distanceBottom >= 10) {
+    Serial.println("---TARGET DETECTED---");
+  }  
+}
+
+
+/**
  * ------- BASIC Movement --------
  */
  
-/**
+/*
  * deg = degrees to turn
  * dist = distance to move (
  */
@@ -187,14 +212,13 @@ int isRed(Color &curColor){
   go(dist);
  }
 
-
 void rotate(int deg){
   //rotate (POS = clockwise, NEG = counter-clockwise)
   Serial.print("Rotating: "); 
   Serial.println(deg);
 
   if (deg > 0 && deg <= 360) {
-    /*
+    /* -> clockwise ->
      * H   |
      * |---|
      * |   H
@@ -209,7 +233,7 @@ void rotate(int deg){
     analogWrite (LEFT_FORWARD, 150);
     analogWrite (LEFT_BACKWARD, 0);     
   } else if (deg < 0 && deg > -360) {
-    /*
+    /* <- counter-clockwise <-
      * |   H
      * |---|
      * H   |
@@ -226,19 +250,12 @@ void rotate(int deg){
   }
 
   delay(STEP5 * (deg/5));
-  
-  analogWrite (RIGHT_FORWARD, 0); 
-  analogWrite (RIGHT_BACKWARD, 0);
-  analogWrite (LEFT_FORWARD, 0);  
-  analogWrite (LEFT_BACKWARD, 0);
-  digitalWrite(RIGHT_FORWARD, LOW); 
-  digitalWrite(RIGHT_BACKWARD, LOW); 
-  digitalWrite(LEFT_FORWARD, LOW); 
-  digitalWrite(LEFT_BACKWARD, LOW);
+ 
+  resetTracks();
 }
-
 void go(int dist) {
-  if (dist) {  
+  if (dist > 0) {  
+    //Forward
     digitalWrite(RIGHT_FORWARD, HIGH); 
     digitalWrite(LEFT_FORWARD, HIGH); 
     digitalWrite(RIGHT_BACKWARD, LOW); 
@@ -258,82 +275,154 @@ void go(int dist) {
     digitalWrite(RIGHT_BACKWARD, LOW); 
     digitalWrite(LEFT_FORWARD, LOW); 
     digitalWrite(LEFT_BACKWARD, LOW);
+  } else if (dist < 0) {
+    //Reverse
+    digitalWrite(RIGHT_FORWARD, LOW); 
+    digitalWrite(LEFT_FORWARD, LOW); 
+    digitalWrite(RIGHT_BACKWARD, HIGH); 
+    digitalWrite(LEFT_BACKWARD, HIGH);
+    analogWrite (RIGHT_BACKWARD, 150); 
+    analogWrite (LEFT_BACKWARD, 150);
+    analogWrite (RIGHT_FORWARD, 0);
+    analogWrite (LEFT_FORWARD, 0);
+    
+    delay(MOVE * dist);
   }
+
+  resetTracks();
+}
+void resetTracks() {
+    analogWrite (RIGHT_FORWARD, 0); 
+    analogWrite (RIGHT_BACKWARD, 0);
+    analogWrite (LEFT_FORWARD, 0);  
+    analogWrite (LEFT_BACKWARD, 0);
+    digitalWrite(RIGHT_FORWARD, LOW); 
+    digitalWrite(RIGHT_BACKWARD, LOW); 
+    digitalWrite(LEFT_FORWARD, LOW); 
+    digitalWrite(LEFT_BACKWARD, LOW); 
 }
 
-/*
- * ------- ADV Movement -------
+void stepOne() {
+    drive(0, 5);
+}
+void rotateOne() {
+    drive (5, 0);
+}
+
+/**
+ * -------- MODES --------
+ */
+ 
+/**
+ * SCAN MODE
  */
 
- //TODO: move along line
+ void scan() {
 
-/*
- * Ball Detection
- */
-
-// Return 1 if found in 360 around robot, else 0
-int findBall(){
-  int found = seeBall();
-  ballRot = 0;
+  while(findRed()){
+    recordLine();  
+  }
   
-  //rotate while checking ball  
-  while(!found || ballRot == 360){
-    drive(5, 0);
-    ballRot += 5;
-    found = seeBall();
+  //find red line
+    //Found: record angle
+    //else: end scan mode
+    //move along red line
+    //when red -> white, record distance
+ }
+
+//Color sensor is on rear of chassis
+//return 1 if found red, if not return 0
+void findRed() {  
+  int found = isRed();
+  redRot = 0;
+  
+  //Rotate to find red
+  while(redRot != 350 || !found){
+    rotateOne();
+    redRot += ROT_UNIT;
+    found = isRed();
   }
 
-  if (found && ballRot < 360) {
+  if (found && redRot < 350) {
     return 1;   
   } else {
     return 0;
   }
 }
 
-//return 1 if radar input sees ball, else 0
-int seeBall(){
-  distanceTop = (ultrasonicTop.distanceRead());
-  distanceBottom = (ultrasonicBottom.distanceRead());
-  printRadar();
-
-  if (distanceTop - distanceBottom >= 10) {
-    return 1;
-  } else {
-    return 0;
-  }
+void recordLine() {
+    int curDistance = 0;
+    //go along line until white
+    while(isRed()) {
+        stepOne();
+        curDistance += DIST_UNIT;
+    }
 }
-
-void printRadar() {
-  Serial.print("TOP: ");
-  Serial.print(distanceTop);
-  Serial.println(" CM");
-  Serial.print("BOTTOM: ");
-  Serial.print(distanceBottom);
-  Serial.println(" CM");
-
-  if (distanceTop - distanceBottom >= 10) {
-    Serial.println("---TARGET DETECTED---");
-  }  
-}
+ 
 
 /**
- * MODES
+ * PLAY MODE
  */
 
- //SCAN
- void scan() {
+ void play(){}
 
-  while(findRed()){
-    recordLine();    
-  }
-  
-  //find red line
-    //Found: record angle
-    //else: end scan mode
-  //move along red line
-    //when red -> white, record distance
-    
- }
+
+
+
+
+
+
+
+
+/**
+ * Delete?
+ */
+
+//
+//void check() {
+//  long distanceBottom = (ultrasonicBottom.distanceRead());
+//  Serial.print("BOTTOM: ");
+//  Serial.print(distanceBottom);
+//  Serial.println(" CM");
+//  long distanceTop = (ultrasonicTop.distanceRead());
+//  Serial.print("TOP: ");
+//  Serial.print(distanceTop);
+//  Serial.println(" CM");
+//  if(distanceTop > distanceBottom){
+//    driveForward();
+//  }
+//}
+
+
+//void driveForward(){
+//	Serial.println("MOVING FORWARD") ;
+//	digitalWrite(RIGHT_FORWARD, HIGH);
+//	digitalWrite(LEFT_FORWARD, HIGH) ;
+//  digitalWrite(RIGHT_BACKWARD, LOW);
+//  digitalWrite(LEFT_BACKWARD, LOW) ;
+//  analogWrite (RIGHT_BACKWARD, 0) ;
+//  analogWrite (LEFT_BACKWARD, 0) ;
+//	analogWrite (RIGHT_FORWARD, 150) ;
+//	analogWrite (LEFT_FORWARD, 150) ;
+//
+//	delay(100);
+//
+//  analogWrite (RIGHT_FORWARD, 0) ;
+//  analogWrite (LEFT_FORWARD, 0) ;
+//  digitalWrite(RIGHT_FORWARD, LOW);
+//  digitalWrite(LEFT_FORWARD, LOW) ;
+//  move();
+//}
+
+//void move() {
+//  for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    Serial.println("HEER");
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(100);                       // waits 15ms for the servo to reach the position
+//  }
+//
+//}
  
 
 
