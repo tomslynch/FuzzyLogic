5dc5b0e514b0c3782584fd85ca0f9f1c4b7e54ab
Thomas Lynch <thomaslynch01@gmail.com>
2017-04-06 22:13:39 -0500

    Basic movement, ball detection, int init cleanup

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -15,24 +15,27 @@
 //TODO: Rotate Hitter
 //TODO: Hit ball
 
-
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
-// to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
-
-Servo myservo;
-int pos = 0;
 struct Color {
   uint16_t r = 0; 
   uint16_t g = 0;
   uint16_t b = 0;
   uint16_t c = 0;
 } color;
+
+/*
+ * Motor init
+ */
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree) to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+
+/* Initialise with specific int time and gain values */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 // Ping Upper = Pin 8,9
 //Ping lower = Pin 10,11
@@ -50,23 +53,28 @@ const int RIGHT_FORWARD = 5;
 const int LEFT_FORWARD = 6;
 const int LEFT_BACKWARD = 7;
 
-/* init color values */
-Color prevColor;  //TODO: setup to check center later
-
 /*
  * Globals
  */
+int MOVE = 500; //delay for moving (delay for half second)
+int STEP5 = 50; //delay scale for rotation (delay time to move 5 degrees)
+
+//servo vars
+Servo myservo;
+int pos = 0;
+
+//ball detection
 long distanceTop;
 long distanceBottom;
 
+//movement track
+int ballRot = 0; //rotation from line to find ball
 
 
 
-  /* Initialise with default values (int time = 2.4ms, gain = 1x) */
-// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
-
-/* Initialise with specific int time and gain values */
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+/*
+ * START
+ */
 
 void setup() {
   Serial.begin(9600);
@@ -95,7 +103,7 @@ void setup() {
 
 void loop() {
  check();
- printColor();
+// printColor();
  delay(1000);
 }
 
@@ -134,8 +142,7 @@ void driveForward(){
   analogWrite (LEFT_FORWARD, 0) ;
   digitalWrite(RIGHT_FORWARD, LOW); 
   digitalWrite(LEFT_FORWARD, LOW) ;
-  move();
-	
+  move();	
 }
 
 void printColor(){
@@ -146,7 +153,6 @@ void printColor(){
  curColor.r - curColor.r/avg;
  curColor.g - curColor.g/avg;
  curColor.b - curColor.b/avg;
-//  getColor(&curColor);
   Serial.print("R: "); Serial.print(curColor.r, DEC); Serial.print(" ");
   Serial.print("G: "); Serial.print(curColor.g, DEC); Serial.print(" ");
   Serial.print("B: "); Serial.print(curColor.b, DEC); Serial.print(" ");
@@ -167,26 +173,134 @@ void move() {
   }
   
 }
-//void getColor(Color &curColor) {
-//  tcs.getRawData(curColor.r, curColor.g, curColor.b, curColor.c);
-//}
+
+/*
+ * ------- BASIC Movement --------
+ */
+ 
+/**
+ * deg = degrees to turn
+ * dist = distance to move (
+ */
+ void drive(int deg, int dist) {
+  rotate(deg);
+  go(dist);
+ }
+
+
+void rotate(int deg){
+  //rotate (POS = clockwise, NEG = counter-clockwise)
+  Serial.print("Rotating: "); 
+  Serial.println(deg);
+
+  if (deg > 0 && deg <= 360) {
+    /*
+     * H   |
+     * |---|
+     * |   H
+     */
+    digitalWrite(RIGHT_FORWARD, LOW); 
+    digitalWrite(RIGHT_BACKWARD, HIGH); 
+    digitalWrite(LEFT_FORWARD, HIGH); 
+    digitalWrite(LEFT_BACKWARD, LOW);
+
+    analogWrite (RIGHT_FORWARD, 150); 
+    analogWrite (RIGHT_BACKWARD, 0);
+    analogWrite (LEFT_FORWARD, 150);
+    analogWrite (LEFT_BACKWARD, 0);     
+  } else if (deg < 0 && deg > -360) {
+    /*
+     * |   H
+     * |---|
+     * H   |
+     */
+    digitalWrite(RIGHT_FORWARD, HIGH); 
+    digitalWrite(RIGHT_BACKWARD, LOW); 
+    digitalWrite(LEFT_FORWARD, LOW); 
+    digitalWrite(LEFT_BACKWARD, HIGH);
+
+    analogWrite (RIGHT_FORWARD, 150); 
+    analogWrite (RIGHT_BACKWARD, 0);
+    analogWrite (LEFT_FORWARD, 150);
+    analogWrite (LEFT_BACKWARD, 0);    
+  }
+
+  delay(STEP5 * (deg/5));
+  
+  analogWrite (RIGHT_FORWARD, 0); 
+  analogWrite (RIGHT_BACKWARD, 0);
+  analogWrite (LEFT_FORWARD, 0);  
+  analogWrite (LEFT_BACKWARD, 0);
+  digitalWrite(RIGHT_FORWARD, LOW); 
+  digitalWrite(RIGHT_BACKWARD, LOW); 
+  digitalWrite(LEFT_FORWARD, LOW); 
+  digitalWrite(LEFT_BACKWARD, LOW);
+}
+
+void go(int dist) {
+  if (dist) {  
+    digitalWrite(RIGHT_FORWARD, HIGH); 
+    digitalWrite(LEFT_FORWARD, HIGH); 
+    digitalWrite(RIGHT_BACKWARD, LOW); 
+    digitalWrite(LEFT_BACKWARD, LOW);
+    analogWrite (RIGHT_BACKWARD, 0); 
+    analogWrite (LEFT_BACKWARD, 0);
+    analogWrite (RIGHT_FORWARD, 150);
+    analogWrite (LEFT_FORWARD, 150);
+    
+    delay(MOVE * dist);
+   
+    analogWrite (RIGHT_FORWARD, 0); 
+    analogWrite (RIGHT_BACKWARD, 0);
+    analogWrite (LEFT_FORWARD, 0);  
+    analogWrite (LEFT_BACKWARD, 0);
+    digitalWrite(RIGHT_FORWARD, LOW); 
+    digitalWrite(RIGHT_BACKWARD, LOW); 
+    digitalWrite(LEFT_FORWARD, LOW); 
+    digitalWrite(LEFT_BACKWARD, LOW);
+  }
+}
+
+/*
+ * ------- ADV Movement -------
+ */
+
+ //TODO: move along line
 
 /*
  * Ball Detection
  */
 
-// Return 0 if found in 360 around robot, else -1
+// Return 1 if found in 360 around robot, else 0
 int findBall(){
-  //rotate while checking ball
+  int found = seeBall();
+  ballRot = 0;
+  
+  //rotate while checking ball  
+  while(!found || ballRot == 360){
+    drive(5, 0);
+    ballRot += 5;
+    found = seeBall();
+  }
+
+  if (found && ballRot < 360) {
+    return 1;   
+  } else {
+    return 0;
+  }
 }
 
-//return 0 if radar input sees ball, else -1
+//return 1 if radar input sees ball, else 0
 int seeBall(){
   distanceTop = (ultrasonicTop.distanceRead());
   distanceBottom = (ultrasonicBottom.distanceRead());
   printRadar();
-  
-  return (distanceTop - distanceBottom >= 10);
+
+  if (distanceTop - distanceBottom >= 10) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 void printRadar() {
